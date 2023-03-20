c660973bfcf5fc607fe88f350ee1014fd42c5c70
MrQuang <qdb611@gmail.com>
2023-03-20 14:28:12 +0700

    init

--- app/routes.php
+++ app/routes.php
@@ -7,15 +7,6 @@ Router::site(function() {
 		Router::get("$pf/doLogout", 'AuthController@doLogout', 'doLogout');
 		Router::post("$pf/doRegistry", 'AuthController@doRegistry', 'doRegistry');
 	});
-
-	Router::group("/gift", function($pf) {
-		Router::post("$pf/turn", 'GiftController@updateTurn', 'updateTurn');
-		Router::post("$pf/hisspin", 'GiftController@popupHistorySpin', 'popupHistorySpin');
-		Router::post("$pf/hisgift", 'GiftController@popupHistoryGift', 'popupHistoryGift');
-		Router::post("$pf/change", 'GiftController@giftChange', 'giftChange');
-		Router::post("$pf/addturn", 'GiftController@addTurnShare', 'addTurnShare');
-	});
-
 	
 	Router::get('/', 'HomeController@index', 'home');
 });
