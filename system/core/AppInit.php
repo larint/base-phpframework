35ab1602d3ea92c4bb66217b0719d88757322949
MrQuang <qdb611@gmail.com>
2023-05-17 20:46:25 +0700

    add db and query

--- system/core/AppInit.php
+++ system/core/AppInit.php
@@ -2,24 +2,24 @@
 
 abstract class AppInit
 {
-
-    public function __construct( $request )
+    public function __construct($request)
     {
         require_once PATH_SYSTEM . '/core/loader/LangLoader.php';
         $lang = new LangLoader();
         $lang->load();
 
-    	require_once PATH_SYSTEM . '/core/loader/ConfigLoader.php';
-    	$this->config = new ConfigLoader($request);
-    	$this->config->load(true);
+        require_once PATH_SYSTEM . '/core/loader/ConfigLoader.php';
+        $this->config = new ConfigLoader($request);
+        $this->config->load(true);
 
         require_once PATH_SYSTEM . '/core/loader/HelperLoader.php';
         $helper = new HelperLoader();
         $helper->load(true);
 
         require_once PATH_SYSTEM . '/core/loader/ModelLoader.php';
-        $this->model = new ModelLoader($request);
-        $this->model->load(true);
+        $model = new ModelLoader($request);
+        $model->load(true);
+        $this->db = new Model();
 
         require_once PATH_SYSTEM . '/core/loader/MiddlewareLoader.php';
         $middleware = new MiddlewareLoader($request);
@@ -28,5 +28,5 @@ abstract class AppInit
         require_once PATH_SYSTEM . '/core/loader/TemplateLoader.php';
         $this->view = new TemplateLoader($request);
     }
-    
+
 }
--- system/core/crud/Model.php
+++ system/core/crud/Model.php
@@ -2,5 +2,19 @@
 
 class Model extends DBCRUD
 {
+    public function query($sql)
+    {
+        $data = [];
+        if ($result = $this->conn->query($sql)) {
+            $this->numRowsEffect = $result->num_rows;
+
+            while ($row = $result->fetch_row()) {
+                $data[] = $row;
+            }
+
+            $result->free_result();
+        }
+        return arr_to_obj($data);
+    }
 
 }
